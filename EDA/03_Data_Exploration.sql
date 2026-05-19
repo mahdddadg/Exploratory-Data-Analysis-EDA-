0359b850aa8342cbb3ce6083bc06277c45941596
mahdi dehlaghi <169053407+mahdddadg@users.noreply.github.com>
2026-05-19 11:06:50 -0500

    Update 03_Data_Exploration.sql

--- EDA/03_Data_Exploration.sql
+++ EDA/03_Data_Exploration.sql
@@ -1,4 +1,29 @@
+/* ============================================================
+   Customer & Sales Time Range Exploration
 
+   Purpose:
+   Explore transactional timelines and customer age ranges
+   to better understand the historical coverage of the dataset
+   and customer demographic information.
+
+   Analysis Includes:
+
+   1. Sales Order Date Analysis
+      - Identify the first recorded order date
+      - Identify the latest recorded order date
+      - Calculate the total time span of sales activity
+        in months
+
+   2. Customer Birthdate Analysis
+      - Identify the oldest customer birthdate
+      - Identify the most recent customer birthdate
+      - Estimate the youngest customer age
+
+
+   Dataset:
+   - gold.facts_sales
+   - gold.dim_customers
+============================================================ */
 --here to exploare , first and las torders and have knowlege abour customers age
 select
 
